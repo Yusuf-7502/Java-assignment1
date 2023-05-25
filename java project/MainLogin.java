074775d5cdd755aa509bb3b1a57ace01e94e8234
Yusuf-7502 <yusufshaik575@gmail.com>
2023-05-25 10:29:29 +0530

    login page

---
+++ java project/MainLogin.java
@@ -0,0 +1,10 @@
+public class MainLogin {
+
+    public static void main(String[] args) {
+
+        IDandPasswords idandPasswords = new IDandPasswords();
+
+        LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());
+
+    }
+}
